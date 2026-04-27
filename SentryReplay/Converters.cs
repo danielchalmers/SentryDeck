1482ee9fa2c6d7a130a4e461f55aa31e21fdfb07
Daniel Chalmers <daniel.chalmers@outlook.com>
2026-04-27 09:01:22 -0500

    Consolidate conevrters into one file

---
+++ SentryReplay/Converters.cs
@@ -0,0 +1,82 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace SentryReplay;
+
+/// <summary>
+/// Converts a boolean to Visibility (true = Visible, false = Collapsed).
+/// </summary>
+public sealed class BoolToVisibilityConverter : MarkupExtension, IValueConverter
+{
+    public override object ProvideValue(IServiceProvider serviceProvider) => this;
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool b)
+        {
+            return b ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        return Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+/// <summary>
+/// Inverts a boolean value.
+/// </summary>
+public sealed class InverseBoolConverter : MarkupExtension, IValueConverter
+{
+    public override object ProvideValue(IServiceProvider serviceProvider) => this;
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool b)
+        {
+            return !b;
+        }
+
+        return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool b)
+        {
+            return !b;
+        }
+
+        return false;
+    }
+}
+
+/// <summary>
+/// Converts a TimeSpan to a formatted string (mm:ss or hh:mm:ss).
+/// </summary>
+public sealed class TimeSpanToStringConverter : MarkupExtension, IValueConverter
+{
+    public override object ProvideValue(IServiceProvider serviceProvider) => this;
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is TimeSpan ts)
+        {
+            return ts.TotalHours >= 1
+                ? ts.ToString(@"h\:mm\:ss")
+                : ts.ToString(@"m\:ss");
+        }
+
+        return "0:00";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
