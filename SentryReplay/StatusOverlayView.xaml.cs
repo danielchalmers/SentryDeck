0c8172e897be15c179e67a0b25292f6a78f1e56d
Daniel Chalmers <daniel.chalmers@outlook.com>
2026-06-03 10:09:51 -0500

    Extract status overlay view

---
+++ SentryReplay/StatusOverlayView.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows.Controls;
+
+namespace SentryReplay;
+
+/// <summary>
+/// Interaction logic for StatusOverlayView.xaml
+/// </summary>
+public partial class StatusOverlayView : UserControl
+{
+    public StatusOverlayView()
+    {
+        InitializeComponent();
+    }
+}
